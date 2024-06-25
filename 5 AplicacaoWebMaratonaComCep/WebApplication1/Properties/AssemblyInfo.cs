8483d1fc01df16feb79e9f9f169280f7dcf8b8f0
luizcarvalho2000 <luiz.fe.carvalho36@gmail.com>
2024-06-25 10:19:27 -0300

    Meu commit

--- 5 AplicacaoWebMaratonaComCep/WebApplication1/Properties/AssemblyInfo.cs
+++ 5 AplicacaoWebMaratonaComCep/WebApplication1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AplicacaoWebMaratona_05_22062024")]
+[assembly: AssemblyTitle("WebApplication1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AplicacaoWebMaratona_05_22062024")]
+[assembly: AssemblyProduct("WebApplication1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bfe779cf-891c-4af2-a331-0c4f9d86f2e7")]
+[assembly: Guid("d4e18e38-9616-4950-baf2-13c9fe074d75")]
 
 // Version information for an assembly consists of the following four values:
 //
